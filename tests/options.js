62a7b5dec2ec4c50393021f8b59a23e43d853d84
Benjamin Beret <benjamin.beret@gmail.com>
2018-09-07 14:34:21 +0200

    options for CI

--- tests/options.js
+++ tests/options.js
@@ -7,14 +7,7 @@ const fs = require('fs');
 const { fromEnv } = require('../dist/security');
 
 const XSD_PATH = process.env.B2B_XSD_PATH || path.join(__dirname, '../b2b-xsd');
-const security = !process.env.CI
-  ? fromEnv()
-  : {
-      cert: Buffer.from(process.env.NM_B2B_PEM || '', 'utf8'),
-      key: Buffer.from(process.env.NM_B2B_PEM_KEY || '', 'utf8'),
-      passphrase: process.env.NM_B2B_PASSPHRASE || null,
-    };
-
+const security = fromEnv();
 const flavour = process.env.B2B_FLAVOUR || 'OPS';
 
 module.exports = {
