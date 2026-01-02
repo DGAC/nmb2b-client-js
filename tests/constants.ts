d91bbb91b42661f995b9746149e7cd7e7b359f5f
Benjamin Beret <benjamin.beret@gmail.com>
2026-01-02 09:22:21 +0100

    Better WSDL handling for unit tests

---
+++ tests/constants.ts
@@ -0,0 +1,2 @@
+export const MOCK_B2B_XSD_ENDPOINT = 'https://fake-b2b.test/xsd';
+export const MOCK_B2B_ENDPOINT = 'https://fake-b2b.test';
--- tests/ensureWSDLPresence.ts
+++ tests/ensureWSDLPresence.ts
@@ -1,7 +1,11 @@
+import assert from 'node:assert';
+import fs from 'node:fs/promises';
+import { getXSDCacheDirectory } from '../src/utils/xsd/paths.js';
 import { WSDLExists } from '../src/utils/xsd/index.js';
 import { resolveB2BEnvironment } from './resolveB2BEnvironment.js';
+import { MOCK_B2B_XSD_ENDPOINT } from './constants.js';
 
-export async function setup() {
+export default async function setup() {
   const env = resolveB2BEnvironment();
 
   const exists = await WSDLExists({
@@ -17,4 +21,54 @@ export async function setup() {
         `Please run 'pnpm downloadWSDL' before launching tests.\n`,
     );
   }
+
+  /**
+   * Ensures WSDL file availability for unit tests without triggering network downloads.
+   *
+   * At this point, we know that we have real WSDL on disk.
+   *
+   * We need to make those available for the unit tests.
+   *
+   * Unit tests use a fake 'xsdEndpoint' URL for configuration validation. This script aliases the cache
+   * directory that the client naturally derives from this URL to the real location of the files
+   * on disk via a symbolic link.
+   */
+  const sourceDir = getXSDCacheDirectory({
+    XSD_PATH: env.B2B_XSD_PATH,
+    xsdEndpoint: env.B2B_XSD_REMOTE_URL,
+  });
+
+  const targetDir = getXSDCacheDirectory({
+    XSD_PATH: env.B2B_XSD_PATH,
+    xsdEndpoint: MOCK_B2B_XSD_ENDPOINT,
+  });
+
+  assert.notEqual(
+    sourceDir,
+    targetDir,
+    'Mocked B2B XSD endpoint should not point to the real WSDL folder',
+  );
+
+  const stats = await fs.lstat(targetDir).catch(() => null);
+
+  if (stats?.isSymbolicLink()) {
+    const symlinkSource = await fs.readlink(targetDir);
+
+    assert.equal(
+      symlinkSource,
+      sourceDir,
+      'WSDL symlink exists but points to an invalid folder.',
+    );
+
+    /**
+     * If a correct symlink already exists, there's no point in cleaning up.
+     */
+    return;
+  }
+
+  await fs.symlink(sourceDir, targetDir, 'dir');
+
+  return async function cleanup() {
+    await fs.unlink(targetDir);
+  };
 }
